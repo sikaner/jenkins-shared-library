bce4124d44bdcdd648f32e6cb63fa5eaa3f729ea
SIKANDAR HAYAT <66342467+sikaner@users.noreply.github.com>
2025-01-14 00:00:14 -0800

    Update modelDeploy.groovy

--- vars/modelDeploy.groovy
+++ vars/modelDeploy.groovy
@@ -1,6 +1,14 @@
-// vars/modelDeploy.groovy
+#!/usr/bin/env groovy
+
 def call(String environment) {
     sh """
+        # Activate virtual environment
+        source \${WORKSPACE}/mldenv/bin/activate
+        
+        # Run deploy script with environment
         python3 src/deploy.py ${environment}
+        
+        # Deactivate virtual environment
+        deactivate
     """
 }
