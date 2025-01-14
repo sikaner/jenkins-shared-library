01fc95991b5d3fbb27ff6cd0e80a0a1d99f2205b
SIKANDAR HAYAT <66342467+sikaner@users.noreply.github.com>
2025-01-14 00:03:16 -0800

    Update modelTest.groovy

--- vars/modelTest.groovy
+++ vars/modelTest.groovy
@@ -1,6 +1,14 @@
-// vars/modelTest.groovy
+#!/usr/bin/env groovy
+
 def call(String environment) {
     sh """
+        # Activate virtual environment
+        source \${WORKSPACE}/mldenv/bin/activate
+        
+        # Run test script
         python3 src/test.py ${environment}
+        
+        # Deactivate virtual environment
+        deactivate
     """
 }
