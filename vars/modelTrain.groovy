b04f85450bdba0274793144cdcedabdcf8a27fb5
SIKANDAR HAYAT <66342467+sikaner@users.noreply.github.com>
2025-01-14 00:02:38 -0800

    Update modelTrain.groovy

--- vars/modelTrain.groovy
+++ vars/modelTrain.groovy
@@ -1,7 +1,15 @@
-// vars/modelTrain.groovy
+#!/usr/bin/env groovy
+
 def call() {
     sh """
-        python3 -m pip install -r requirements.txt
+        # Activate virtual environment
+        source \${WORKSPACE}/mldenv/bin/activate
+        
+        # Install requirements and run training
+        pip install -r requirements.txt
         python3 src/train.py
+        
+        # Deactivate virtual environment
+        deactivate
     """
 }
